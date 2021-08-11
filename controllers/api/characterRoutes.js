1244489219ae0d18887306d00d95574c38dbf893
Dylan Barstow <dcbarstow7@gmail.com>
2021-08-10 20:47:43 -0500

    Creation of connection, home routes, index, api: character route, index, user, and home

---
+++ controllers/api/characterRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Character } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newCharacter = await Character.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newCharacter);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const characterData = await Character.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!characterData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(characterData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
