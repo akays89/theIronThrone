c18df8d642a241f0e9196ca8dda09f8f69a85234
Dylan Barstow <dcbarstow7@gmail.com>
2021-08-10 20:51:25 -0500

    fixed message

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -25,7 +25,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!characterData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+      res.status(404).json({ message: 'No character found with this id!' });
       return;
     }
 
