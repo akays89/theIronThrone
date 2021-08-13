d515c4a7b8ba75f3d1782653d7bf868fd36c543b
A Fish <ashley.paz.fish@gmail.com>
2021-08-13 11:19:36 -0500

    update to routes and packag.json settings and setup server.js

---
+++ controllers/questionRoutes.js
@@ -0,0 +1,42 @@
+const questData = require('../seeds/questionsData.json');
+const router = require('express').Router();
+
+// Pulls just the first question
+const quest1 = questData[0].text;
+// Pulls first answer
+const answer = questData[0].responses[0].answer;
+// Pulls the first answer's value
+const answerValue = questData[0].responses[0].value;
+
+const responses = [];
+
+
+// Pulls each question and the list of answers
+// for (let i = 0; i < questData.length; i++) {
+//     const questions = questData[i].text;
+//     console.log(questions);
+
+//     for (let t = 0; t < questData[i].responses.length; t++) {
+//         const answers = questData[i].responses[t].answer;
+//         console.log(answers);
+//     }
+//     return;
+// };
+
+// GET questions
+router.get('/', async (req, res) => {
+    try {
+  
+      const questions = questData.map((question) =>
+        question.get({ plain: true })
+      );
+      res.render('questions', {
+        questions,
+        loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+    console.log(questions);
+  });
---
+++ server.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds (86400 === 1 day)
+    maxAge: 86400,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
+});
