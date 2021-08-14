5b3656422bb9fc1c55a5780089cf9557515e99f8
A Fish <ashley.paz.fish@gmail.com>
2021-08-14 09:31:48 -0500

    add .js to the end of path for require

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const characterRoutes = require('./characterRoutes');
+const userRoutes = require('./userRoutes.js');
+const characterRoutes = require('./characterRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/character', characterRoutes);
