99019acaa55dcfcb6dbf0d4ca276baf176e2bee3
A Fish <ashley.paz.fish@gmail.com>
2021-08-18 20:10:04 -0500

    removed /signup from router.post for create user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const bcrypt = require('bcrypt');
 
-router.post('/signup', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
